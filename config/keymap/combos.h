c0d96f2d8f279c1ac88c1f8fbba6672e8e0020e9
Jonathan Feinberg <jonathf@gmail.com>
2024-03-04 14:51:27 +0100

    juggle buttons

--- config/keymap/combos.h
+++ config/keymap/combos.h
@@ -1,19 +1,24 @@
 #pragma once
 
-#define _combo_q(layer, key) ZMK_COMBO(_Q_ ## layer, key, _UL3 _UL4, layer)
-_combo_q(0, _Q)
-_combo_q(1, _1_EXCL)
-_combo_q(2, _1_PIPE)
-_combo_q(4, _Q)
-_combo_q(5, _0_EXCL)
-_combo_q(6, _1_PIPE)
+/* #define _combo_q(layer, key) ZMK_COMBO(_Q_ ## layer, key, _UL3 _UL4, layer) */
+/* _combo_q(0, _Q) */
+/* _combo_q(1, _1_EXCL) */
+/* _combo_q(2, _1_PIPE) */
+/* _combo_q(4, _Q) */
+/* _combo_q(5, _0_EXCL) */
+/* _combo_q(6, _1_PIPE) */
 
 /* ZMK_COMBO(_COMBO_AE_0, _1_AE,    2  3, 0 1 2) */
 /* ZMK_COMBO(_COMBO_AE_3, _0_AE,    2  3, 4 5 6) */
 
+
+#define combo_oe(layer, key) ZMK_COMBO(_OE_ ## layer, key, _UL1 _UL2, layer)
+combo_oe(0, _1_OE)
+combo_oe(4, _0_OE)
+
 #define combo_aa(layer, key) ZMK_COMBO(_AA_ ## layer, key, _UL2 _UL3, layer)
 combo_aa(0, _1_AA)
-combo_aa(3, _0_AA)
+combo_aa(4, _0_AA)
 
 #define combo_z(layer, key) ZMK_COMBO(_Z_ ## layer, key, _LL3 _LL4, layer)
 combo_z(0, _Z)
@@ -37,13 +42,9 @@ combo_n(5, _N0)
 combo_ae(0, _1_AE)
 combo_ae(4, _0_AE)
 
-#define combo_oe(layer, key) ZMK_COMBO(_OE_ ## layer, key, _UR2 _UR3, layer)
-combo_ae(0, _1_OE)
-combo_ae(4, _0_OE)
-
 ZMK_COMBO(_ESC,   &kp ESC,    _ML3 _ML4, 0 4)
 ZMK_COMBO(_TAB,   &kp TAB,    _ML2 _ML3, 0 4)
-ZMK_COMBO(_INS,   &kp INS,    _UR3 _UR4, 1 5)
+ZMK_COMBO(_INS,   &kp INS,    _UR3 _UR4, 0 4)
 ZMK_COMBO(_DEL,   &kp DEL,    _UR3 _UR4, 0 4)
 ZMK_COMBO(_BKSP,  &kp BKSP,   _MR3 _MR4, 0 4)
 ZMK_COMBO(_ENTER, &kp ENTER,  _MR2 _MR3, 0 4)
