8444ef610da8b262e7bf78ee4ab5b3e6e192838e
Jonathan Feinberg <jonathf@gmail.com>
2025-08-05 08:54:59 +0200

    back to iso us-intl

--- config/keymap/1234.h
+++ config/keymap/1234.h
@@ -21,7 +21,7 @@
 #define _N8 &kp N8
 #define _N9 &kp N9
 
-#define _0_AA &kp LA(A)
+#define _0_AA &kp RA(W)
 #define _1_AA &kp LBKT
 ZMK_UNICODE_PAIR(__2_AA, N0, N0, E, N5, N0, N0, C, N5)
 #define _2_AA &__2_AA
@@ -44,7 +44,7 @@ ZMK_UNICODE_PAIR(__2_AA, N0, N0, E, N5, N0, N0, C, N5)
 #define _N5 &kp N5
 #define _N6 &kp N6
 
-#define _0_OE &kp LA(O)
+#define _0_OE &kp RA(L)
 #define _1_OE &kp SEMI
 ZMK_UNICODE_PAIR(__2_OE, N0, N0, F, N8, N0, N0, D, N8)
 #define _2_OE &__2_OE
@@ -70,7 +70,7 @@ ZMK_UNICODE_PAIR(__2_OE, N0, N0, F, N8, N0, N0, D, N8)
 #define _N2 &hold_tap LCTRL N2
 #define _N3 &hold_tap LALT N3
 
-#define _0_AE &kp LA(SQT)
+#define _0_AE &kp RA(Z)
 #define _1_AE &hold_tap RALT SQT
 ZMK_UNICODE_PAIR(__2_AE, N0, N0, E, N6, N0, N0, C, N6)
 #define _2_AE &__2_AE
