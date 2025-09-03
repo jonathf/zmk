90b578ada66fc302b363cc3d7aa656dc3cb90752
Jonathan Feinberg <jonathf@gmail.com>
2025-09-03 12:20:30 +0200

    streamline

--- config/common.h
+++ config/common.h
@@ -1,5 +1,28 @@
 #define _ &none
 
+#define _LEFT_HAND \
+  LT0 LT1 LT2 LT3 LT4 \
+  LM0 LM1 LM2 LM3 LM4 \
+  LB1 LB2 LB3
+#define _RIGHT_HAND \
+  RT0 RT1 RT2 RT3 RT4 \
+  RM0 RM1 RM2 RM3 RM4 \
+  RB1 RB2 RB3
+
+#define LAYER_A1 0
+#define LAYER_A2 1
+#define LAYER_B1 2
+#define LAYER_B2 3
+#define LAYER_C1 4
+#define LAYER_C2 5
+#define LAYER_D1 6
+#define LAYER_E1 7
+
+#define LAYER_US 0 2 4
+#define LAYER_NO 1 3 5
+#define LAYER_ALL 0 1 2 3 4 5 6 7
+
+
 ZMK_HOLD_TAP(holdtap_left,
   flavor = "balanced";
   require-prior-idle-ms = <100>;
